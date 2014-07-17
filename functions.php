06219916db49198a3f619572540742f695f04479
Juan Correa <jc@raya.cl>
2014-07-17 15:34:14 -0400

    Modificamos información que no debería de estar. Además, corregimos las dependencias (enganchando el main.js al plugins.js)

--- functions.php
+++ functions.php
@@ -2,14 +2,15 @@
 
 function skeletoing_load_styles() {
     wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.min.css', '1' );
-	wp_enqueue_style( 'pak-operadores-css', get_stylesheet_uri(), '2', array('normalize') );
+	wp_enqueue_style( 'skeletoing-style', get_stylesheet_uri(), '2', array('normalize') );
 }
 
 function skeletoing_load_scripts(){
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . "/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js", array(), '2.6.2');
     //wp_enqueue_script( 'jquery-1-10-google', "//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js", array(), '1.10', true);
-    wp_enqueue_script( 'plugins-js', get_template_directory_uri() . "/js/plugins.js", array(), '000', true);
-    wp_enqueue_script( 'main-js', get_template_directory_uri() . "/js/main.js", array(), '000', true);
+    wp_enqueue_script('jquery');
+	wp_enqueue_script( 'plugins-js', get_template_directory_uri() . "/js/plugins.js", array(), '000', true);
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . "/js/main.js", array('plugins-js'), '000', true);
 }
 
 function skeletoing_init(){
